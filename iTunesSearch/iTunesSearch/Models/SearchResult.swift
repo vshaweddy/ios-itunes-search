de07f2812bd9b649a1dd394ff35ba1c252b0fe0f
Vici Shaweddy <vshaweddy@gmail.com>
2019-09-07 20:03:35 -0400

    Part 1 - Search Result

---
+++ iTunesSearch/iTunesSearch/Models/SearchResult.swift
@@ -0,0 +1,24 @@
+//
+//  SearchResult.swift
+//  iTunesSearch
+//
+//  Created by Vici Shaweddy on 9/6/19.
+//  Copyright © 2019 Vici Shaweddy. All rights reserved.
+//
+
+import Foundation
+
+struct SearchResult: Codable {
+    let title: String
+    let creator: String
+    
+    enum CodingKeys: String, CodingKey {
+        case title = "trackName"
+        case creator = "artistName"
+    }
+}
+
+// allow us to decode the JSON data into this object, then access the actual search results through its results property
+struct SearchResults {
+    let results: [SearchResult]
+}
