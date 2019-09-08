a0b027f2fd8562aeeaa3f50359834dbf06c1a14d
Vici Shaweddy <vshaweddy@gmail.com>
2019-09-07 20:05:22 -0400

    Part 1 - ResultType

---
+++ iTunesSearch/iTunesSearch/Models/ResultType.swift
@@ -0,0 +1,15 @@
+//
+//  ResultType.swift
+//  iTunesSearch
+//
+//  Created by Vici Shaweddy on 9/7/19.
+//  Copyright © 2019 Vici Shaweddy. All rights reserved.
+//
+
+import Foundation
+
+enum ResultType: String {
+    case software
+    case musicTrack
+    case movie
+}
